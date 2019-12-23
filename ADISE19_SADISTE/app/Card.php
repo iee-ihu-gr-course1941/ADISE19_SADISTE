81b9cb4a2a8815a55cab93658b057d09d97e3774
Dimitris Tzikas <dimitristzikas@protonmail.com>
2019-12-23 11:43:45 +0200

    add Enums and card class

---
+++ ADISE19_SADISTE/app/Card.php
@@ -0,0 +1,46 @@
+<?php
+
+include 'Enum/CardColor.php';
+include 'Enum/CardType.php';
+
+
+class Card 
+{
+
+    private $color;
+    private $type;
+
+    public function __construct($color, $type)
+    {
+        if(CardColor::isValidName($color))
+        {
+            $this->color = $color;
+        }
+        else
+        {
+            throw new Exception("Invalid color");
+        }
+
+        if(CardType::isValidName($type))
+        {
+            $this->type = $type;
+        }
+        else
+        {
+            throw new Exception("Invalid type");
+        }
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }    
+
+}
+
+?>
---
+++ ADISE19_SADISTE/app/Enum/CardColor.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once "BasicEnum.php";
+
+abstract class CardColor extends BasicEnum
+{
+    const red = 0;
+    const green = 1;
+    const blue = 2;
+    const yellow = 3;
+    const black = 4;
+}
+
+?>
