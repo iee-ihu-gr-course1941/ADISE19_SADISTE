8368c5ed3c7e0837d32bb0c35207e4d705f62553
Theodoros Balasis <theodorosbalasis@gmail.com>
2020-01-01 20:46:04 +0200

    Implement enum iteration.

--- ADISE19_SADISTE/app/Enum/BasicEnum.php
+++ ADISE19_SADISTE/app/Enum/BasicEnum.php
@@ -7,20 +7,19 @@ use ReflectionClass;
 /**
  * Abstract class from which Enum classes derive. Provides helper functions for handling enums.
  */
-abstract class BasicEnum
-{
+abstract class BasicEnum {
 
     /**
      * An array mapping BasicEnum derived classes to an array of their constants.
      */
-    private static $constCacheArray = null;
+    private static $constCacheArray;
 
     /**
      * Gets an array of the constants defined in the class this is called from.
      *
      * @return A key-value array of the class' constants.
      */
-    private static function getConstants()
+    private static function getConstants(): array
     {
         if (self::$constCacheArray === null) {
             self::$constCacheArray = [];
@@ -43,9 +42,8 @@ abstract class BasicEnum
      * @param boolean $strict Whether to perform a case-sensitive check.
      * @return boolean Whether the key exists.
      */
-    public static function isValidName($name, $strict = false)
-    {
-		$constants = self::getConstants();
+    public static function isValidName(string $name, bool $strict = false): bool {
+        $constants = self::getConstants();
 
         if ($strict) {
             return array_key_exists($name, $constants);
@@ -58,13 +56,16 @@ abstract class BasicEnum
     /**
      * Checks if a given value exists in the constants of the class this is called from.
      *
-     * @param object $value The value to check for.
+     * @param $value The value to check for.
      * @param boolean $strict Whether to perform a type-strict check.
      * @return boolean Whether the value exists.
      */
-    public static function isValidValue($value, $strict = true)
-    {
+    public static function isValidValue($value, bool $strict = true): bool {
         $values = array_values(self::getConstants());
         return in_array($value, $values, $strict);
+    }
+
+    public static function getIterator(): EnumIterator {
+        return new EnumIterator(self::getConstants());
     }
 }
